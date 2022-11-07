b860d455729048e0e2165faac55efd02e43d456f
KimGeunSoo <geunsoo1106@gmail.com>
2022-11-07 19:30:23 +0900

    파이썬 기초 독학 시작

---
+++ index.py
@@ -0,0 +1,34 @@
+# 숫자
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*5)
+print(3*(3+1))
+# 문자
+print('풍선')
+print("나비")
+print("두진이형 원빈닮음"*3)
+# 참/거짓
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not (5>10))
+# True
+
+# 애완동물을 소개해주삼
+name = "나비"
+animal = "고양이"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리 집 " + animal + " 이름 " + name )
+hobby = "츄르"
+print(name,"는 ",str(age),"살, " +hobby+"을 좋아함")
+# ,쓰면 빈칸이 들어감
+print(name+"는 어른임? "+str(is_adult))
+
